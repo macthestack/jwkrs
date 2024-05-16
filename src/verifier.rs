f5756d8825d49ef6200c899eee61c6988f8fa486
Martin Svensson <hetast@gmail.com>
2024-05-16 13:27:42 +0200

    updated claims struct

--- src/verifier.rs
+++ src/verifier.rs
@@ -1,23 +1,65 @@
+use core::fmt;
+
 use crate::config::JwkConfiguration;
 use crate::key::Key;
 use evmap::ReadHandleFactory;
 use jsonwebtoken::decode;
 use jsonwebtoken::decode_header;
 use jsonwebtoken::TokenData;
+use serde::de;
+use serde::de::Visitor;
 use serde::Deserialize;
+use serde::Deserializer;
+use tracing::trace;
 
 #[derive(Debug, Deserialize)]
 pub struct Claims {
-    // The audience the token was issued for
-    pub aud: Vec<String>,
-    // The expiry date -- as epoch seconds
+    pub aud: Audience,
     pub exp: i64,
-    // The token issuer
     pub iss: String,
-    // The subject the token refers to
     pub sub: String,
-    // Issued at -- as epoch seconds
     pub iat: i64,
+}
+
+#[derive(Debug)]
+pub enum Audience {
+    Single(String),
+    Multiple(Vec<String>),
+}
+
+impl<'de> Deserialize<'de> for Audience {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct AudienceVisitor;
+
+        impl<'de> Visitor<'de> for AudienceVisitor {
+            type Value = Audience;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("a string or a list of strings")
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<Audience, E>
+            where
+                E: de::Error,
+            {
+                Ok(Audience::Single(value.to_owned()))
+            }
+
+            fn visit_seq<A>(self, seq: A) -> Result<Audience, A::Error>
+            where
+                A: de::SeqAccess<'de>,
+            {
+                let values: Vec<String> =
+                    Deserialize::deserialize(de::value::SeqAccessDeserializer::new(seq))?;
+                Ok(Audience::Multiple(values))
+            }
+        }
+
+        deserializer.deserialize_any(AudienceVisitor)
+    }
 }
 
 #[derive(Debug)]
@@ -57,7 +99,9 @@ impl<'a> JwkVerifier<'a> {
         validator: &Key,
         token: &str,
     ) -> Result<TokenData<Claims>, VerificationError> {
-        return decode::<Claims>(token, &validator.key, &validator.validation)
-            .map_err(|_| VerificationError::InvalidSignature);
+        return decode::<Claims>(token, &validator.key, &validator.validation).map_err(|e| {
+            trace!("Failed to decode token: {:?}", e);
+            VerificationError::InvalidSignature
+        });
     }
 }
